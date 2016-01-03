f3ec8bd2ae3959c8bada047c996b3fd914dbc8e0
Asim <code.learner@outlook.com>
2016-01-03 20:43:29 +0530

    Added view for password change.

--- database_app/urls.py
+++ database_app/urls.py
@@ -15,15 +15,24 @@ Including another URLconf
     3. Add a URL to urlpatterns:  url(r'^blog/', include(blog_urls))
 """
 from django.conf.urls import url, include
-from .views import Db_Login_View, Db_Logout_View
+#from django.contrib.auth.views import password_change, logout, password_change_done
+#from django.core.urlresolvers import reverse, reverse_lazy
+from .views import Db_Login_View, Db_Logout_View, Password_Change_View
 from . import views
+
+	
 urlpatterns = [
 	url(r'^$', Db_Login_View.as_view(), name = 'db_login'),
 	url(r'^landing/$', views.landing, name = 'landing'),
+	#url(r'^landing/$', Db_Landing_View.as_view(), name = 'landing'),
 	url(r'^details/$', views.details, name = 'details'),
 	url(r'^profile/$', views.profile, name = 'profile'),
 	url(r'^stat/$', views.stat, name = 'stat'),
 	url(r'^search_results/$', views.landing, name = 'search_results'),
 	url(r'^edit/$', views.edit, name = 'edit'),
 	url(r'^logout/$', Db_Logout_View.as_view(), name = 'db_logout'),
+	#url(r'^logout/$', logout, name = 'db_logout'),
+	url(r'^password_change/$', Password_Change_View.as_view(), name = 'password_change'),
+	#url(r'^password_change/$', password_change, {'template_name':'database_app/password_change_form.html', 'post_change_redirect':'/landing/?password_change=success'},name = 'change_password'),
+	#url(r'^password_change/done/$', password_change_done, name='password_change_done'),
 ]
--- database_app/views.py
+++ database_app/views.py
@@ -1,20 +1,23 @@
 from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render, render_to_response, redirect
-from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth import authenticate, login, logout, update_session_auth_hash
 from django.template import RequestContext
 from django.core.urlresolvers import reverse, reverse_lazy
-from django.contrib.auth.forms import AuthenticationForm
+from django.contrib.auth.forms import AuthenticationForm, PasswordChangeForm
 from django.contrib.auth.decorators import login_required
 from django.views.generic.edit import FormView
-from django.views.generic.base import RedirectView
+from django.views.generic.base import RedirectView, View
 from django.views.generic.list import ListView
 from django.utils.decorators import method_decorator
+from django.contrib.auth.mixins import LoginRequiredMixin
 from django.http import QueryDict
+from django.contrib.auth.views import password_change
+#from django.contrib.auth import User
 
 def build_url_with_get(*args, **kwargs):
 	params = kwargs.pop('params', {})
-	url = reverse(*args, **kwargs)
-	
+	url = reverse_lazy(*args, **kwargs)
+	print(url)
 	if not params:
 		return url
 
@@ -56,7 +59,8 @@ class Db_Login_View(FormView):
 		else:
 			return reverse_lazy('database_app:landing')
 
-class Db_Logout_View(RedirectView):
+class Db_Logout_View(LoginRequiredMixin, RedirectView):
+	login_url = reverse_lazy('database_app:db_login')
 	
 	def get(self, request, *args, **kwargs):
 		logout(request)
@@ -65,10 +69,59 @@ class Db_Logout_View(RedirectView):
 	def get_redirect_url(self, *args, **kwargs):
 		url = build_url_with_get('database_app:db_login', params = {'logout':'success'})
 		return url
+
+class Password_Change_View(LoginRequiredMixin, FormView):
+	login_url = reverse_lazy('database_app:db_login')
+	
+	template_name = 'database_app/password_change_form.html'
+	form_class = PasswordChangeForm
+	#a_url = reverse_lazy('database_app:landing')
+	#url = build_url_with_get('database_app:landing', params = {'password_change':'success'})
+	#print(a_url)
+	
+	def get_form_kwargs(self):
+		kwargs = super(Password_Change_View, self).get_form_kwargs()
+		user = self.request.user
+		kwargs['user'] = user
 		
+		return kwargs
+	
+	def form_valid(self, form):
+		print('form_valid called')
+		form.save()
+		# Updating the password logs out all other sessions for the user
+		# except the current one if
+		# django.contrib.auth.middleware.SessionAuthenticationMiddleware
+		# is enabled.
+		update_session_auth_hash(self.request, form.user)
+		return super(Password_Change_View, self).form_valid(form)
+		
+	def get_success_url(self):
+		next_url = build_url_with_get('database_app:landing', params = {'password_change':'success'})
+		return next_url
+	
+	
+#@method_decorator(login_required, name = 'dispatch')
+# class Db_Landing_View(LoginRequiredMixin, ListView):
+	# login_url = reverse_lazy('database_app:db_login')
+	
+	# def get_menu_items(self, user):
+		# menu_item = {
+						# 'user': user.get_full_name(),
+						# 'logout_url': reverse_lazy('database_app:db_logout'),
+						# 'change_password_url': reverse_lazy('database_app:change_password'),
+		# }
+		
+			
 @login_required()
 def landing(request):
-	return render(request, 'database_app/landing.html')
+	context = {}
+	
+	if 'password_change' in request.GET:
+		if request.GET['password_change'].lower() == 'success':
+				custom_message = 'Password changed successfully.'
+				context['custom_message'] = custom_message
+	return render(request, 'database_app/landing.html', context)
 
 @login_required()
 def details(request):
